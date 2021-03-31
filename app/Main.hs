4eb899d809ad1f7e50c8537ea456017ce287b0f8
Abastro <abab9579@naver.com>
2021-03-31 21:29:42 +0900

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main :: IO ()
+main = xmonad def
