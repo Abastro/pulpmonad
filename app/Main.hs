463c7e752254676b11f6056b73868b7ede6adaac
Abastro <abab9579@naver.com>
2021-04-05 00:42:54 +0900

    Stable

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,4 @@
+{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 module Main ( main ) where
 
 import Data.List
@@ -5,13 +6,12 @@ import qualified Data.Map as M
 import XMonad
 import qualified XMonad.StackSet as W
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks ( docks, avoidStruts )
-import XMonad.Hooks.EwmhDesktops ( ewmh, fullscreenEventHook )
+import XMonad.Hooks.EwmhDesktops ( fullscreenEventHook )
 import XMonad.Actions.MouseResize ( mouseResize )
-import XMonad.Actions.SpawnOn ( spawnOn )
 import XMonad.Util.Run ( spawnPipe, safeSpawn, safeSpawnProg )
 import XMonad.Util.EZConfig ( additionalKeys, additionalMouseBindings )
-import XMonad.Config.Gnome ( gnomeConfig )
+import XMonad.Config.Desktop ( desktopConfig )
+import XMonad.Config.Gnome ( gnomeRegister )
 import System.IO ( hPutStrLn )
 import Text.Printf ( printf )
 
@@ -34,7 +34,7 @@ tray = "trayer" <-| [
     "--edge top", "--align right", "--expand true"
   , "--SetDockType true", "--SetPartialStrut true"
   , "--transparent true", "--alpha 0", "--tint 0x000000"
-  , "--width 5", "--height 18"
+  , "--width 10", "--height 24"
   ]
 statBar = "xmobar" <-| [ mkPath [pathCfg, ".xmobar"] ]
 
@@ -53,12 +53,14 @@ main = do
   spawn "killall trayer"; spawn tray
   -- Maybe status here?
   xmbar <- spawnPipe statBar
-  -- TODO Perhaps just use gnome-panel?
-  xmonad $ gnomeConfig {
+  let config = desktopConfig
+  xmonad $ config {
     focusedBorderColor = "#eeaaaa"
   , normalBorderColor = "#cccccc"
-  , workspaces = ["main", "code", "term", "4", "pic", "6", "7", "8", "9"]
-  , startupHook = startupHook gnomeConfig >> do
+  , workspaces = ["main", "code", "term", "chat", "pic", "6", "7", "8", "9"]
+  , terminal = console
+  , startupHook = startupHook config <+> do
+      gnomeRegister -- Registers xmonad with gnome
       spawn $ mkPath ["$HOME", ".xmonad", "xmonad.hook"]
   , manageHook = composeAll $ [
       className =? "Gimp" --> doF (W.shift "pic")
@@ -68,13 +70,13 @@ main = do
         "Chat", "Participants", "Rooms", "Poll", "float"] <$> title) --> doFloat
     , className =? "Gnome-calculator" --> doFloat
     , className =? "Eog" --> doFloat
-    ] <> [ manageHook gnomeConfig ]
-  , layoutHook = mouseResize $ layoutHook gnomeConfig
+    ] <> [ manageHook config ]
+  , layoutHook = mouseResize $ layoutHook config
   , logHook = dynamicLogWithPP xmobarPP {
       ppOutput = hPutStrLn xmbar
     , ppTitle  = xmobarColor "#555555" "" . shorten 50
     }
-  , handleEventHook = handleEventHook gnomeConfig <> fullscreenEventHook
+  , handleEventHook = handleEventHook config <> fullscreenEventHook
   , modMask = superMask
   } `additionalMouseBindings` concat [ mouseMove ]
     `additionalKeys` concat [ keysUtility, keysBasic, keysScreenshot ]
@@ -95,7 +97,8 @@ main = do
       ]
     keysBasic = [
         ((superMask, xK_p), spawn "dmenu_run")
-      , ((superMask, xK_Pause), safeSpawnProg logout)
+      , ((superMask, xK_Pause), safeSpawn logout ["--no-prompt", "--logout"])
+      , ((superMask, xK_Delete), safeSpawn logout ["--no-prompt", "--power-off"])
       ]
     keysScreenshot = [
         ((noModMask, xK_Print), spawn "sleep 0.2; gnome-screenshot")
