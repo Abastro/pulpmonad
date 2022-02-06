2fcd4ea0e6433b0f6fa644ac00a952b98a955ab7
Abastro <abab9579@gmail.com>
2022-02-06 23:42:17 +0900

    Build script changes

--- bar/Main.hs
+++ bar/Main.hs
@@ -1,17 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import GI.Gtk hiding (main)
-import System.Directory
-import System.FilePath
-import System.Taffybar
-import System.Taffybar.Context (TaffyIO)
-import System.Taffybar.Information.CPU
-import System.Taffybar.Information.Memory
-import System.Taffybar.SimpleConfig hiding (barPadding)
-import System.Taffybar.Widget
-import System.Taffybar.Widget.Generic.Icon
-import System.Taffybar.Widget.Generic.PollingBar
-import Text.Printf
+import           GI.Gtk                  hiding ( main )
+import           System.Directory
+import           System.FilePath
+import           System.Taffybar
+import           System.Taffybar.Context        ( TaffyIO )
+import           System.Taffybar.Information.CPU
+import           System.Taffybar.Information.Memory
+import           System.Taffybar.SimpleConfig
+                                         hiding ( barPadding )
+import           System.Taffybar.Widget
+import           System.Taffybar.Widget.Generic.Icon
+import           System.Taffybar.Widget.Generic.PollingBar
+import           Text.Printf
 
 cpuCallback :: IO Double
 cpuCallback = do
@@ -19,22 +20,16 @@ cpuCallback = do
   return totalLoad
 
 cpuImg :: FilePath -> TaffyIO Widget
-cpuImg home = pollingIconImageWidgetNew
-  (cpuN 0)
-  0.1
-  $ do
-    cpu <- cpuCallback
-    pure (cpuN . round $ cpu * 5)
-  where
-    cpuN :: Int -> FilePath
-    cpuN n = home </> printf ".xmonad/asset/icons/cpu%d.png" n
+cpuImg home = pollingIconImageWidgetNew (cpuN 0) 0.1 $ do
+  cpu <- cpuCallback
+  pure (cpuN . round $ cpu * 5)
+ where
+  cpuN :: Int -> FilePath
+  cpuN n = home </> printf ".xmonad/asset/icons/cpu%d.png" n
 
 memCfg :: BarConfig
 memCfg =
-  (defaultBarConfig $ const (0.1, 0.6, 0.9))
-    { barWidth = 25,
-      barPadding = 4
-    }
+  (defaultBarConfig $ const (0.1, 0.6, 0.9)) { barWidth = 8, barPadding = 1 }
 
 memCallback :: IO Double
 memCallback = memoryUsedRatio <$> parseMeminfo
@@ -43,25 +38,18 @@ main :: IO ()
 main = do
   home <- getHomeDirectory
   let memimg = iconImageWidgetNew $ home </> ".xmonad/asset/icons/ram.png"
-  startTaffybar $
-    toTaffyConfig
-      defaultSimpleTaffyConfig
-        { startWidgets = [workspaces],
-          centerWidgets = [clock],
-          endWidgets = [sniTrayNew, mem, memimg, cpuImg home, batteryIconNew],
-          barPosition = Top,
-          barHeight = 40,
-          cssPath = Just $ home </> ".xmonad/styles/taffybar.css"
-        }
-  where
-    clock =
-      textClockNewWith
-        defaultClockConfig
-          { clockFormatString = "%a %b %_d %H:%M %p"
-          }
-    mem = pollingBarNew memCfg 0.5 memCallback
-    workspaces =
-      workspacesNew
-        defaultWorkspacesConfig
-          { showWorkspaceFn = hideEmpty
-          }
+  startTaffybar $ toTaffyConfig defaultSimpleTaffyConfig
+    { startWidgets  = [workspaces]
+    , centerWidgets = [clock]
+    , endWidgets    = [sniTrayNew, mem, memimg, cpuImg home, batteryIconNew]
+    , barPosition   = Top
+    , barHeight     = 40
+    , cssPath       = Just $ home </> ".xmonad/styles/taffybar.css"
+    }
+ where
+  clock = textClockNewWith defaultClockConfig
+    { clockFormatString = "%a %b %_d %H:%M %p"
+    }
+  mem = pollingBarNew memCfg 0.5 memCallback
+  workspaces =
+    workspacesNew defaultWorkspacesConfig { showWorkspaceFn = hideEmpty }
