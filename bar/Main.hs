fbeb36ffb1092d6885a432b5c9a0f7842118b1f9
Abastro <abab9579@gmail.com>
2022-02-12 15:48:32 +0900

    Rudimentary memory bar

--- bar/Main.hs
+++ bar/Main.hs
@@ -19,29 +19,40 @@ cpuCallback = do
   (_, _, totalLoad) <- cpuLoad
   return totalLoad
 
-cpuImg :: FilePath -> TaffyIO Widget
-cpuImg home = pollingIconImageWidgetNew (cpuN 0) 0.1 $ do
+cpuWidget :: FilePath -> TaffyIO Widget
+cpuWidget home = pollingIconImageWidgetNew (cpuN 0) 0.1 $ do
   cpu <- cpuCallback
   pure (cpuN . round $ cpu * 5)
  where
   cpuN :: Int -> FilePath
   cpuN n = home </> printf ".xmonad/asset/icons/cpu%d.png" n
 
-memCfg :: BarConfig
-memCfg =
-  (defaultBarConfig $ const (0.1, 0.6, 0.9)) { barWidth = 8, barPadding = 1 }
-
 memCallback :: IO Double
 memCallback = memoryUsedRatio <$> parseMeminfo
+
+memWidget :: FilePath -> TaffyIO Widget
+memWidget home = do
+  bg  <- iconImageWidgetNew memN
+  bar <- pollingBarNew memCfg 0.5 memCallback
+  wid <- overlayNew
+
+  containerAdd wid bar
+  overlayAddOverlay wid bg
+  widgetShowAll wid
+  toWidget wid
+ where
+  memN = home </> ".xmonad/asset/icons/ram.png"
+  memCfg =
+    (defaultBarConfig $ const (0.1, 0.6, 0.9)) { barWidth = 21, barPadding = 7 }
+
 
 main :: IO ()
 main = do
   home <- getHomeDirectory
-  let memimg = iconImageWidgetNew $ home </> ".xmonad/asset/icons/ram.png"
   startTaffybar $ toTaffyConfig defaultSimpleTaffyConfig
     { startWidgets  = [workspaces]
     , centerWidgets = [clock]
-    , endWidgets    = [sniTrayNew, mem, memimg, cpuImg home, batteryIconNew]
+    , endWidgets = [sniTrayNew, memWidget home, cpuWidget home, batteryIconNew]
     , barPosition   = Top
     , barHeight     = 45
     , cssPath       = Just $ home </> ".xmonad/styles/taffybar.css"
@@ -50,6 +61,5 @@ main = do
   clock = textClockNewWith defaultClockConfig
     { clockFormatString = "%a %b %_d %H:%M %p"
     }
-  mem = pollingBarNew memCfg 0.5 memCallback
   workspaces =
     workspacesNew defaultWorkspacesConfig { showWorkspaceFn = hideEmpty }
