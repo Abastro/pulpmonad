1364f1a3510d31602cf74e76fcd08ecb6040bcc2
Abastro <abab9579@gmail.com>
2022-03-28 12:29:29 +0900

    Config rearrange

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,8 @@
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module Main (main) where
 
-import Data.List
 import Data.Map qualified as M
-import Data.Maybe
-import Graphics.X11.ExtraTypes.XF86
-import Text.Printf (printf)
+import System.Directory
+import System.FilePath
 import XMonad
 import XMonad.Actions.MouseResize (mouseResize)
 import XMonad.Config.Desktop (desktopConfig)
@@ -21,91 +17,75 @@ import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Tabbed
 import XMonad.StackSet qualified as W
-import XMonad.Util.EZConfig (additionalKeys, additionalMouseBindings)
+import XMonad.Util.EZConfig (additionalKeysP, additionalMouseBindings)
 import XMonad.Util.Run (safeSpawn, safeSpawnProg)
 import XMonad.Util.Themes
 
-mkPath :: [FilePath] -> FilePath
-mkPath = intercalate "/"
-
-pathCfg, pathAs :: FilePath
-pathCfg = mkPath ["$HOME", ".xmonad", "cfg"]
-pathAs = mkPath ["$HOME", ".xmonad", "asset"]
-
-(<-|) :: String -> [String] -> String
-pr <-| opts = unwords (pr : opts)
-
-(-:), (--:), (=:) :: String -> String -> String
-opt --: val = printf "--%s %s" opt val
-opt -: val = printf "-%s %s" opt val
-opt =: val = printf "%s=\"%s\"" opt val
-
-background, tray, statBar :: String
-background = "feh" <-| ["bg-scale" --: mkPath [pathAs, "Background.jpg"]]
-tray =
-  "trayer"
-    <-| [ "--edge top",
-          "--align right",
-          "--expand true",
-          "--SetDockType true",
-          "--SetPartialStrut true",
-          "--transparent true",
-          "--alpha 0",
-          "--tint 0x000000",
-          "--width 10",
-          "--height 35"
-        ]
-statBar = "xmobar" <-| [mkPath [pathCfg, ".xmobar"]]
-
-console, browser, logout :: String
-console = "gnome-terminal"
-browser = "nautilus"
-logout = "gnome-session-quit"
-
-leftClick, rightClick, middleClick :: Button
-(leftClick, rightClick, middleClick) = (button1, button3, button2)
-
-altMask, superMask :: KeyMask
-(altMask, superMask) = (mod1Mask, mod4Mask)
-
-myTabCfg = (theme adwaitaDarkTheme) {decoHeight = 50}
+middleClick :: Button -- Leftclick = button1, Rightclick = button3
+middleClick = button2
 
 myLayout =
   onWorkspaces ["code", "pics"] (tabbed shrinkText myTabCfg) $
     tall ||| wide ||| myTab
   where
+    myTabCfg = (theme adwaitaDarkTheme) {decoHeight = 50}
     tall = Tall 1 (3 / 100) (1 / 2)
     wide = Mirror (Tall 1 (3 / 100) (1 / 2))
     myTab = tabbed shrinkText myTabCfg
 
 main :: IO ()
 main = do
-  spawn background
-  let config = ewmh desktopConfig
+  xmDir <- (</> ".xmonad") <$> getHomeDirectory
+  safeSpawn "feh" ["--bg-scale", xmDir </> "asset" </> "Background.jpg"]
+  let cfg = ewmh desktopConfig
+      mouseMove =
+        [((controlMask, middleClick), \w -> isFloating w --> (focus w >> kill))]
+      keysUtility =
+        [ ("M-M1-h", safeSpawn "xdg-open" [xmDir </> "asset" </> "Xmbindings.png"]),
+          ("M-d", safeSpawnProg "nautilus"),
+          ("M-M1-s", safeSpawnProg "/usr/local/pulse/pulseUi")
+        ]
+      keysBasic =
+        [ ("M-p", safeSpawnProg "synapse"),
+          ("M-M1-<Delete>", safeSpawn "systemctl" ["poweroff"]),
+          ("<XF86MonBrightnessUp>", safeSpawn "lux" ["-a", "5%"]),
+          ("<XF86MonBrightnessDown>", safeSpawn "lux" ["-s", "5%"]),
+          ("<XF86AudioRaiseVolume>", safeSpawn "pactl" ["set-sink-volume", "0", "+5%"]),
+          ("<XF86AudioLowerVolume>", safeSpawn "pactl" ["set-sink-volume", "0", "-5%"]),
+          ("<XF86AudioMute>", safeSpawn "pactl" ["set-sink-mute", "0", "toggle"])
+        ]
+      keysScreenshot =
+        [ ("<Print>", spawn "sleep 0.2; gnome-screenshot"),
+          ("M1-<Print>", spawn "sleep 0.2; gnome-screenshot -w"),
+          ("C-<Print>", spawn "gnome-screenshot -i")
+        ]
+      keysSpecial =
+        [("M-M1-d", debugStack)]
+
   xmonad . ewmhFullscreen . pagerHints $
-    config
+    cfg
       { focusedBorderColor = "#eeaaaa",
         normalBorderColor = "#cccccc",
         workspaces = ["main", "docs", "code", "term", "chat", "pics", "7", "8", "9"],
-        terminal = console,
+        terminal = "gnome-terminal",
         startupHook =
-          startupHook config <+> do
+          startupHook cfg <> do
             setWMName "LG3D"
             gnomeRegister -- Registers xmonad with gnome
-            spawn $ mkPath ["$HOME", ".xmonad", "xmonad.hook"],
-        manageHook = staticManage <> manageHook config,
+            safeSpawnProg (xmDir </> "xmonad.hook"),
+        manageHook = staticManage <> manageHook cfg,
         layoutHook = mouseResize . smartBorders . avoidStruts $ myLayout,
-        handleEventHook = handleEventHook config,
-        modMask = superMask
+        handleEventHook = handleEventHook cfg,
+        modMask = mod4Mask -- Super key
       }
       `additionalMouseBindings` mouseMove
-      `additionalKeys` concat [keysUtility, keysBasic, keysSpecial, keysScreenshot]
+      `additionalKeysP` concat [keysUtility, keysBasic, keysSpecial, keysScreenshot]
   where
     staticManage =
       composeAll
         [ resource =? "synapse" --> doIgnore,
           className =? "Gimp" --> doF (W.shift "pics"),
-          (role =? "gimp-toolbox" <||> role =? "gimp-image-window") --> unFloat,
+          role =? "gimp-toolbox" <||> role =? "gimp-image-window" --> unFloat,
           className =? "zoom" <&&> (not <$> (title =? "Zoom" <||> title =? "Zoom Meeting")) --> doFloat,
           className =? "Soffice" <&&> isFullscreen --> doFullFloat,
           className =? "Gnome-calculator" --> doFloat,
@@ -123,35 +103,7 @@ main = do
         a <- getAtom "_NET_WM_WINDOW_TYPE"
         t <- getAtom typ
         long <- io $ getWindowProperty32 d a w
-        pure $ t `elem` (fromIntegral <$> fromMaybe [] long)
+        pure $ t `elem` maybe [] (map fromIntegral) long
 
     unFloat = ask >>= doF . W.sink
     isFloating = \w -> M.member w . W.floating <$> gets windowset
-
-    mouseMove =
-      [ -- Mod(Super) + Left Click: Float & Move around
-        -- Mod(Super) + Right Click: Float & Resize
-        ((controlMask, middleClick), \w -> isFloating w --> (focus w >> kill))
-      ]
-    keysUtility =
-      [ ((superMask .|. altMask, xK_h), spawn $ "xdg-open" <-| [mkPath [pathAs, "Xmbindings.png"]]),
-        ((superMask, xK_d), safeSpawnProg browser),
-        ((superMask .|. altMask, xK_s), spawn "/usr/local/pulse/pulseUi")
-      ]
-    keysBasic =
-      [ ((superMask, xK_p), spawn "synapse"),
-        -- Super + Shift + Q: Logout
-        ((superMask .|. altMask, xK_Delete), spawn "systemctl poweroff"),
-        ((noModMask, xF86XK_MonBrightnessUp), safeSpawn "lux" ["-a", "5%"]),
-        ((noModMask, xF86XK_MonBrightnessDown), safeSpawn "lux" ["-s", "5%"]),
-        ((noModMask, xF86XK_AudioRaiseVolume), safeSpawn "pactl" ["set-sink-volume", "0", "+5%"]),
-        ((noModMask, xF86XK_AudioLowerVolume), safeSpawn "pactl" ["set-sink-volume", "0", "-5%"]),
-        ((noModMask, xF86XK_AudioMute), safeSpawn "pactl" ["set-sink-mute", "0", "toggle"])
-      ]
-    keysScreenshot =
-      [ ((noModMask, xK_Print), spawn "sleep 0.2; gnome-screenshot"),
-        ((altMask, xK_Print), spawn "sleep 0.2; gnome-screenshot -w"),
-        ((controlMask, xK_Print), spawn "gnome-screenshot -i")
-      ]
-    keysSpecial =
-      [((superMask .|. altMask, xK_d), debugStack)]
--- bar/Main.hs
+++ bar/Main.hs
@@ -1,23 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Control.Monad
-import qualified Data.Map                      as M
-import           Data.Maybe
-import           GI.Gdk
-import           GI.Gtk                  hiding ( main )
-import           System.Directory
-import           System.FilePath
-import           System.Taffybar
-import           System.Taffybar.Context        ( TaffyIO )
-import           System.Taffybar.Information.CPU
-import           System.Taffybar.Information.Memory
-import           System.Taffybar.SimpleConfig
-                                         hiding ( barPadding )
-import           System.Taffybar.Widget
-import           System.Taffybar.Widget.Generic.Icon
-import           System.Taffybar.Widget.Generic.PollingBar
-import           Text.Printf
-import           XMonad.Core
+import Control.Monad
+import Data.Map qualified as M
+import Data.Maybe
+import GI.Gdk
+import GI.Gtk hiding (main)
+import System.Directory
+import System.FilePath
+import System.Taffybar
+import System.Taffybar.Context (TaffyIO)
+import System.Taffybar.Information.CPU
+import System.Taffybar.Information.Memory
+import System.Taffybar.SimpleConfig hiding
+  ( barPadding,
+  )
+import System.Taffybar.Widget
+import System.Taffybar.Widget.Generic.Icon
+import System.Taffybar.Widget.Generic.PollingBar
+import Text.Printf
+import XMonad.Core
 
 spawnOnClick :: String -> EventButton -> IO Bool
 spawnOnClick str btn = do
@@ -30,7 +31,7 @@ batWidget = do
   disp <- batteryIconNew
 
   -- Add button events
-  ev   <- eventBoxNew
+  ev <- eventBoxNew
   containerAdd ev disp
   onWidgetButtonReleaseEvent ev $ spawnOnClick "gnome-control-center power"
 
@@ -56,9 +57,9 @@ cpuWidget home = do
 
   widgetShowAll ev
   toWidget ev
- where
-  cpuN :: Int -> FilePath
-  cpuN n = home </> printf ".xmonad/asset/icons/cpu%d.png" n
+  where
+    cpuN :: Int -> FilePath
+    cpuN n = home </> printf ".xmonad/asset/icons/cpu%d.png" n
 
 memCallback :: IO Double
 memCallback = memoryUsedRatio <$> parseMeminfo
@@ -66,8 +67,8 @@ memCallback = memoryUsedRatio <$> parseMeminfo
 memWidget :: FilePath -> TaffyIO Widget
 memWidget home = do
   -- Foreground and the Bar
-  fg      <- iconImageWidgetNew memN
-  bar     <- pollingBarNew memCfg 0.5 memCallback
+  fg <- iconImageWidgetNew memN
+  bar <- pollingBarNew memCfg 0.5 memCallback
   barCtxt <- widgetGetStyleContext bar
   styleContextAddClass barCtxt "mem-bar"
 
@@ -83,39 +84,45 @@ memWidget home = do
 
   widgetShowAll ev
   toWidget ev
- where
-  memN = home </> ".xmonad/asset/icons/ram.png"
-  memCfg =
-    (defaultBarConfig $ const (0.1, 0.6, 0.9)) { barWidth = 9, barPadding = 0 }
+  where
+    memN = home </> ".xmonad/asset/icons/ram.png"
+    memCfg =
+      (defaultBarConfig $ const (0.1, 0.6, 0.9)) {barWidth = 9, barPadding = 0}
 
 workspaceMaps :: M.Map String String
-workspaceMaps = M.fromList
-  [ ("main", "\xe3af")
-  , ("docs", "\xf0c7")
-  , ("code", "\xf121")
-  , ("term", "\xf120")
-  , ("chat", "\xf4ad")
-  , ("pics", "\xf03e")
-  ]
-
+workspaceMaps =
+  M.fromList
+    [ ("main", "\xe3af"),
+      ("docs", "\xf0c7"),
+      ("code", "\xf121"),
+      ("term", "\xf120"),
+      ("chat", "\xf4ad"),
+      ("pics", "\xf03e")
+    ]
 
 main :: IO ()
 main = do
   home <- getHomeDirectory
-  startTaffybar $ toTaffyConfig defaultSimpleTaffyConfig
-    { startWidgets  = [workspaces]
-    , centerWidgets = [clock]
-    , endWidgets    = [sniTrayNew, memWidget home, cpuWidget home, batWidget]
-    , barPosition   = Top
-    , barHeight     = 45
-    , cssPath       = Just $ home </> ".xmonad/styles/taffybar.css"
-    }
- where
-  clock = textClockNewWith defaultClockConfig
-    { clockFormatString = "%a %b %_d %H:%M %p"
-    }
-  getName n = fromMaybe n $ workspaceMaps M.!? n
-  workspaces = workspacesNew defaultWorkspacesConfig
-    { showWorkspaceFn = hideEmpty
-    , labelSetter     = pure . getName . workspaceName
-    }
+  startTaffybar $
+    toTaffyConfig
+      defaultSimpleTaffyConfig
+        { startWidgets = [workspaces],
+          centerWidgets = [clock],
+          endWidgets = [sniTrayNew, memWidget home, cpuWidget home, batWidget],
+          barPosition = Top,
+          barHeight = 45,
+          cssPath = Just $ home </> ".xmonad/styles/taffybar.css"
+        }
+  where
+    clock =
+      textClockNewWith
+        defaultClockConfig
+          { clockFormatString = "%a %b %_d %H:%M %p"
+          }
+    getName n = fromMaybe n $ workspaceMaps M.!? n
+    workspaces =
+      workspacesNew
+        defaultWorkspacesConfig
+          { showWorkspaceFn = hideEmpty,
+            labelSetter = pure . getName . workspaceName
+          }
