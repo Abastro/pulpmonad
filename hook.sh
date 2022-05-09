61c652182dd1b1716cb3e87d74ef62b42f67870a
Abastro <abab9579@gmail.com>
2022-05-09 16:31:31 +0900

    Avoid restarting the programs

--- hook.sh
+++ hook.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
-status-notifier-watcher &
-pasystray &
-nm-applet --sm-disable --indicator &
-blueman-applet &
-synapse -s &
+
+echo "Starting hooks"
+
+if [ -z "$(pgrep status-notifier)" ] ; then
+  status-notifier-watcher &
+fi
+
+if [ -z "$(pgrep pasystray)" ] ; then
+  pasystray &
+fi
+
+if [ -z "$(pgrep nm-applet)" ] ; then
+  nm-applet --sm-disable --indicator &
+fi
+
+if [ -z "$(pgrep blueman-applet)" ] ; then
+  blueman-applet &
+fi
+
+if [ -z "$(pgrep synapse)" ] ; then
+  synapse -s &
+fi
 
 # SSH key
 export SSH_ASKPASS="/usr/bin/ssh-askpass"
--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# TODO Fix mysterious crash.. :/
+# Seems like stuff is not working
+
 config="$XMONAD_CONFIG_DIR"
 cache="$XMONAD_CACHE_DIR"
 xmonad_name="xmonad-$ENV_ARCH-$ENV_OS"
@@ -9,5 +13,7 @@ export GTK2_RC_FILES="$xdgcfg/gtk-2.0/.gtkrc-2.0"
 chmod +x "$config/hook.sh"
 "$config/hook.sh" &
 
-echo "Running XMonad"
-exec "$cache/$xmonad_name" > "$XMONAD_LOG_DIR/xmonad.log" 2> "$XMONAD_LOG_DIR/xmonad.err"
+cp -T "$XMONAD_LOG_DIR/xmonad.log" "$XMONAD_LOG_DIR/xmonad-1.log"
+cp -T "$XMONAD_LOG_DIR/xmonad.err" "$XMONAD_LOG_DIR/xmonad-1.err"
+
+"$cache/$xmonad_name" > "$XMONAD_LOG_DIR/xmonad.log" 2> "$XMONAD_LOG_DIR/xmonad.err"
