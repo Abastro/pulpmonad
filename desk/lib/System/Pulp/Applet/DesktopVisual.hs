f076c0eb8b8ebeddda3d626721ee82e0f04500d3
Abastro <abab9579@gmail.com>
2022-06-06 23:04:59 +0900

    Stop querying images directly from classes

--- desk/lib/System/Pulp/Applet/DesktopVisual.hs
+++ desk/lib/System/Pulp/Applet/DesktopVisual.hs
@@ -9,7 +9,6 @@ module System.Pulp.Applet.DesktopVisual (
   deskVisualizer,
   defImageSetter,
   appInfoImageSetter,
-  classImageSetter,
   defShowFn,
 ) where
 
@@ -19,16 +18,14 @@ import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Maybe
 import Data.Maybe
-import Data.Monoid
 import Data.Text qualified as T
 import GI.GLib (castTo)
 import GI.Gio.Interfaces.AppInfo
 import GI.Gio.Objects.DesktopAppInfo
-import GI.Gtk.Objects.IconTheme qualified as UI
 import Status.X11.WMStatus
 import System.Pulp.Applet.DesktopVisual.Handle
+import UI.Pixbufs qualified as UI
 import View.Imagery
-import qualified UI.Pixbufs as UI
 
 -- MAYBE: https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-1.5.html
 -- TODO Optimize this one, maybe with caching
@@ -46,20 +43,15 @@ appInfoImageSetter WindowInfo{..} = do
 
     filterCond classes deskInfo = do
       wmClass <- either (const Nothing) Just <$> tryAny (desktopAppInfoGetStartupWmClass deskInfo)
-      -- Some slips through, so check it against desktop id with lowercased classes
+      -- Some slips through, so check it against executable & desktop id with lowercased classes
+      exec <- either (const Nothing) Just <$> tryAny (appInfoGetExecutable deskInfo)
+      let exeName = exec >>= fmap T.pack . listToMaybe . words
       ident <- appInfoGetId deskInfo
-      let isMatch cl = Just cl == wmClass || T.toLower cl <> T.pack ".desktop" == ident
+      let isMatch cl =
+            Just cl == wmClass
+              || Just (T.toLower cl) == exeName
+              || T.toLower cl <> T.pack ".desktop" == T.toLower ident
       pure (any isMatch classes)
-
-classImageSetter :: WindowInfo -> MaybeT IO ImageSet
-classImageSetter WindowInfo{windowClasses} = do
-  iconTheme <- UI.iconThemeGetDefault
-  iconName <- getAlt $ foldMap Alt $ findIcon iconTheme <$> windowClasses
-  pure (ImgSName iconName)
-  where
-    findIcon iconTheme className = do
-      UI.iconThemeHasIcon iconTheme className >>= guard
-      pure className
 
 -- MAYBE Proper logging
 xIconImageSetter :: WindowInfo -> GetXIcon -> MaybeT IO ImageSet
@@ -76,7 +68,6 @@ defImageSetter winInfo getXIcon = do
   imageSet <-
     runMaybeT $
       appInfoImageSetter winInfo
-        <|> classImageSetter winInfo
         <|> xIconImageSetter winInfo getXIcon
   pure (fromMaybe (ImgSName $ T.pack "missing") imageSet)
 
