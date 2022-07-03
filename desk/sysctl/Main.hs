1a300f02189da0c48db283012808ebab327dc319
Abastro <abab9579@gmail.com>
2022-07-03 22:34:42 +0900

    Overloading

--- desk/sysctl/Main.hs
+++ desk/sysctl/Main.hs
@@ -3,9 +3,10 @@
 module Main (main) where
 
 import Control.Monad
+import Data.GI.Base.Attributes
+import Data.GI.Base.Constructible
 import Data.Text qualified as T
 import Defines
-import GI.Gdk.Structs.EventKey qualified as Gdk
 import GI.Gtk.Objects.Label qualified as Gtk
 import Gtk.Application qualified as Gtk
 import Gtk.Commons qualified as Gtk
@@ -17,8 +18,6 @@ import System.Exit
 import View.Boxes qualified as View
 import View.Imagery qualified as View
 import XMonad.Util.Run (safeSpawn)
-import Data.GI.Base.Attributes
-import Data.GI.Base.Constructible
 
 -- TODO Internalize to the pulp-taskbar
 
@@ -53,10 +52,10 @@ actOf :: Gtk.Window -> SysCtl -> IO ()
 actOf window = \case
   Build -> do
     safeSpawn "gnome-terminal" ["--class=term-float", "--", "xmonad-manage", "build", "pulpmonad"]
-    Gtk.windowClose window -- For now, close the window..
+    #close window -- For now, close the window..
   Refresh -> do
     safeSpawn "xmonad" ["--restart"]
-    Gtk.windowClose window
+    #close window
   Logout -> safeSpawn "killall" ["xmonad-manage"] -- A roundabout
   Reboot -> safeSpawn "systemctl" ["reboot"]
   Poweroff -> safeSpawn "systemctl" ["poweroff"]
@@ -69,10 +68,10 @@ ctlButton window ctl = do
         [ View.imageStaticNew Gtk.IconSizeDialog True (View.ImgSName $ iconOf ctl)
         , Gtk.toWidget =<< new Gtk.Label [#label := nameOf ctl]
         ]
-  Gtk.widgetSetValign box Gtk.AlignCenter
+  #setValign box Gtk.AlignCenter
 
   btn <- Gtk.buttonNewWith (Just box) $ actOf window ctl
-  Gtk.widgetGetStyleContext btn >>= flip Gtk.styleContextAddClass (styleOf ctl)
+  #getStyleContext btn >>= flip #addClass (styleOf ctl)
   pure btn
 
 main :: IO ()
@@ -85,9 +84,9 @@ main = do
   where
     cssProv :: IO Gtk.CssProvider
     cssProv = do
-      css <- Gtk.cssProviderNew
+      css <- new Gtk.CssProvider []
       cfgDir <- getEnv "XMONAD_DATA_DIR"
-      Gtk.cssProviderLoadFromPath css $ T.pack (cfgDir </> "styles" </> "pulp-sysctl.css")
+      #loadFromPath css $ T.pack (cfgDir </> "styles" </> "pulp-sysctl.css")
       pure css
 
     activating :: Gtk.Application -> IO ()
@@ -95,30 +94,34 @@ main = do
       cssProv >>= flip Gtk.defScreenAddStyleContext Gtk.STYLE_PROVIDER_PRIORITY_USER
 
       window <- Gtk.appWindowNew app
-      Gtk.windowSetTitle window (T.pack "Pulp System Control")
-      Gtk.windowSetDefaultSize window 560 140
-      Gtk.windowSetTypeHint window Gtk.WindowTypeHintSplashscreen
-      Gtk.windowSetPosition window Gtk.WindowPositionCenter
-      Gtk.windowSetKeepAbove window True
-      Gtk.windowSetSkipPagerHint window True
-      Gtk.windowSetSkipTaskbarHint window True
+      #setDefaultSize window 560 140
+      set
+        window
+        [ #title := T.pack "Pulp System Control"
+        , #typeHint := Gtk.WindowTypeHintSplashscreen
+        , #windowPosition := Gtk.WindowPositionCenter
+        , #skipPagerHint := True
+        , #skipTaskbarHint := True
+        ]
+      #setKeepAbove window True
+
       Gtk.onWidgetKeyPressEvent window $
-        Gdk.getEventKeyKeyval >=> \case
-          Gtk.KEY_Escape -> True <$ Gtk.windowClose window
+        flip get #keyval >=> \case
+          Gtk.KEY_Escape -> True <$ #close window
           _ -> pure False
 
       Gtk.windowSetTransparent window
       Gtk.windowGrabOnMap window
 
-      Gtk.containerAdd window =<< btns window
-      Gtk.widgetShowAll window
+      #add window =<< btns window
+      #showAll window
 
     btns :: Gtk.Window -> IO Gtk.Widget
     btns window = do
       box <-
         View.boxStaticNew (View.defBoxArg Gtk.OrientationHorizontal){View.boxSpacing = 10, View.boxHomogeneous = True}
           =<< traverse (ctlButton window) [minBound .. maxBound]
-      Gtk.widgetSetName box (T.pack "pulp-sysctl")
-      Gtk.widgetGetStyleContext box >>= flip Gtk.styleContextAddClass (T.pack "btn-area")
-      Gtk.widgetSetHalign box Gtk.AlignFill
+      #setName box (T.pack "pulp-sysctl")
+      #getStyleContext box >>= flip #addClass (T.pack "btn-area")
+      #setHalign box Gtk.AlignFill
       pure box
