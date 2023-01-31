50cb00d48b589d97f8ddd46b885fe87b42d189e8
Abastro <abab9579@gmail.com>
2023-02-01 00:41:43 +0900

    Testing?

--- desk/test/Spec.hs
+++ desk/test/Spec.hs
@@ -1,15 +1,42 @@
 module Main (main) where
 
 import Control.Monad
+import Reactive.Banana.Combinators hiding (once)
+import Reactive.Banana.Frameworks
+import Status.AudioStatus
 import Status.HWStatus
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
 import Test.QuickCheck.Monadic
-import Status.AudioStatus
+import Control.Event.Entry
+import Data.Maybe
+
+-- | Make future event into plain - also introduces delay to event
+asPlain :: Event (Future a) -> MomentIO (Event a)
+asPlain eFuture = do
+  (ePlain, handle) <- newEvent
+  reactimate' $ fmap handle <$> eFuture
+  pure ePlain
+
+networkDiffEvent :: a -> Event a -> MomentIO (Event (a, a))
+networkDiffEvent x0 ex = do
+  bx  <- stepper x0 ex
+  -- No need to check arbitrary function due to parametricity, I believe
+  asPlain =<< updateEvent (,) bx bx
 
 main :: IO ()
 main = hspec $ do
+  describe "Control.Event.Entry" $ do
+    describe "diffEvent" $ do
+      prop "reports all changes correctly" $ \x mayXs -> monadicIO $ do
+        mayYs <- run $ interpretFrameworks (networkDiffEvent @Int x) mayXs
+        -- Collapse maybes to ignore delays
+        let xs = catMaybes mayXs
+            expected = zip (x : xs) xs
+            actual = catMaybes mayYs
+        assert $ expected == actual
+
   describe "HWStatus" $ do
     describe "cpuStat" $ do
       it "does not throw" $ void cpuStat
