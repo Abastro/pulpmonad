c28a0c2b94fd09aea7b2c6bd34ce03c5cc50b84c
Abastro <abab9579@gmail.com>
2023-06-29 22:07:31 +0900

    Also installs fonts now.

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 cfg="$XMONAD_CONFIG_DIR"
 
-# TODO Fonts?
+# Install fonts
+echo "* Installing fonts..."
+cp -R "$cfg/asset/fonts" "$HOME/.local/share"
 
 # Copy greeter badge in place
 if [ -d "/usr/share/slick-greeter/badges" ]; then
-  echo "Inserting greeter badges."
+  echo "* Inserting greeter badges..."
   sudo cp "$cfg/asset/badge/pulpmonad.svg" "/usr/share/slick-greeter/badges"
   sudo chmod a+r "/usr/share/slick-greeter/badges/pulpmonad.svg"
 fi
