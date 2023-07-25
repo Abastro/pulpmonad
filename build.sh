3b18c76d013bde4b09a2e720df123e29db862394
Abastro <abab9579@gmail.com>
2023-07-25 10:43:35 +0900

    Font moved away, removed prefer-dark-theme

--- build.sh
+++ build.sh
@@ -15,9 +15,7 @@ ln -sf "$XMONAD_CACHE_DIR/$PROF_ID" "$XMONAD_CACHE_DIR/$XMONAD_NAME"
 
 # Copy configurations
 xdgcfg="$HOME/.config"
-cp -T "$config/cfg/.gtkrc-2.0" "$xdgcfg/gtk-2.0/.gtkrc-2.0"
 cp -T "$config/cfg/gtk3-settings.ini" "$xdgcfg/gtk-3.0/settings.ini"
-cp -T "$config/cfg/gtk4-settings.ini" "$xdgcfg/gtk-4.0/settings.ini"
 
 # Copy assets & styles
 cp -R "$config/asset" "$data"
